76e2a59c7b28b0184169990eb902bcee2c1e5fae
Galax Gao <gaoxiaowei2117@gmail.com>
2026-09-09 13:55:00 -0700

    chore: update customer support email

--- frontend/src/config/support.ts
+++ frontend/src/config/support.ts
@@ -1,2 +1,2 @@
 /** Single source of truth for the customer-support contact shown in the UI. */
-export const SUPPORT_EMAIL = 'gaoxwc@126.com'
+export const SUPPORT_EMAIL = 'gaoxiaowei2117@gmail.com'
